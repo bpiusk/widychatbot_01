8984a05d44f122823c6de9514d83050271668674
Benediktus Pius Kanyan <bpiuskanyan@gmail.com>
2025-06-05 06:58:58 +0700

    terjadi perubahan karena adanya tags pada file pdf

--- backend/utils/splitter.py
+++ backend/utils/splitter.py
@@ -1,8 +1,8 @@
-# utils/splitter.py
 import re
 
 def split_text(raw_text):
     
-    pattern = r'(Pertanyaan:.*?Jawaban:.*?)(?=Pertanyaan:|$)'
+    pattern = r'(Pertanyaan:.*?Jawaban:.*?(?:Tag:.*?)*)(?=Pertanyaan:|$)'
+
     chunks = re.findall(pattern, raw_text, re.DOTALL)
     return [chunk.strip() for chunk in chunks if chunk.strip()]
