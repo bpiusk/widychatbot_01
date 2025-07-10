b12da1634ac8d842c6e1b84494e09c2be233c9fd
Benediktus Pius Kanyan <bpiuskanyan@gmail.com>
2025-07-10 18:04:39 +0700

    -

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://widychatbot-01.vercel.app",
-        "https://widychatbot-01-cg2sxck89-bene30s-projects.vercel.app",
+        "https://widychatbot-01-ou91waxrl-bene30s-projects.vercel.app",
         "http://localhost:3000" 
     ],
     allow_credentials=True,
